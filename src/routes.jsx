b6f5738f796cebeb34db7f03795df5f864f91698
dinarastamkulova <dinarastamkulova@Users-MacBook-Pro.local>
2023-02-19 23:44:42 +0600

    fix router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import { Experience } from "./components/Experience";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -7,9 +7,9 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route index path="/" element={<Experience />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={Experience}/>
+      </Switch>
       <Footer/>
     </BrowserRouter>
   );
